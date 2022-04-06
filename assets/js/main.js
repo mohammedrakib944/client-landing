137358d8f5d08ee7499ec268fbf5644b9096350d
mohammedrakib944 <mohammedrakib944@gmail.com>
2022-04-06 12:22:52 +0600

    client landing update

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,7 @@
 window.onload = () => {
   setTimeout(() => {
     document.querySelector(".Loader").style.display = "none";
-  }, 1500);
+  }, 100);
 };
 
 function showMenu() {
@@ -15,27 +15,9 @@ function hideMenu() {
 // nav bar added
 window.onscroll = function () {
   let POS_Y = window.pageYOffset;
-  document.querySelector(".straight-line").style.height = POS_Y + "px";
-
   const navi = document.querySelector(".navigation");
   let height = window.pageYOffset;
   if (height >= 50) navi.classList.add("afterScroll");
   else navi.classList.remove("afterScroll");
+  document.querySelector(".straight-line").style.height = POS_Y + "px";
 };
-
-// swiper js
-var swiper = new Swiper(".mySwiper", {
-  slidesPerView: 4,
-  spaceBetween: 30,
-  slidesPerGroup: 1,
-  loop: true,
-  loopFillGroupWithBlank: true,
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-});
